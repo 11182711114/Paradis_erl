60302bcf24aa8bbd8957ae3bd2f43d0f537c8de9
Fredrik <flarsson93@gmail.com>
2018-02-08 02:28:11 +0100

    stuffs

--- W03/advanced/02/hof.erl
+++ W03/advanced/02/hof.erl
@@ -6,30 +6,31 @@ getTimeStamp() ->
     {Mega, Sec, Micro} = os:timestamp(),
     (Mega*1000000 + Sec)*1000 + round(Micro/1000).
 
-%* This assumes that the specified length(L) to MaxWorkers ratio makes sense, i.e. >= 1.
+%* This assumes that the length(L) to MaxWorkers ratio makes sense, i.e. >= 1.
 pmap_timeout(F, L, Timeout, MaxWorkers) ->
-    TimeoutAbs = getTimeStamp()+Timeout,
+    TimeoutTimeStamp = getTimeStamp()+Timeout,
     SplitList = list_op:splitList(L, MaxWorkers),
-    spawn_worker(F, SplitList, TimeoutAbs, self()).
+    spawn_worker(F, SplitList, TimeoutTimeStamp, self()).
 
-gather(_, Count, Count) -> [];
-gather(TimeoutAbs, Count, Final) ->
-    Timeleft = TimeoutAbs - getTimeStamp(),
+gather(_TimeoutTimeStamp, Count, Count) -> [];
+gather(TimeoutTimeStamp, Count, Final) ->
+    TmpRem = TimeoutTimeStamp - getTimeStamp(),
+    Timeleft = if TmpRem>=0 -> TmpRem; true -> 0 end,
     receive
         {Count, Res} ->
-            [Res|gather(TimeoutAbs, Count+1, Final)]
+            [Res|gather(TimeoutTimeStamp, Count+1, Final)]
         after Timeleft ->
             timeout
     end.
 
-spawn_worker(F, SplitList, Timeout, Pid) ->
-    spawn_worker(F, SplitList, Timeout, Pid, 0).
+spawn_worker(F, SplitList, Pid) ->
+    spawn_worker(F, SplitList, Pid, 0).
 
-spawn_worker(_F, [], _Timeout, _Pid, _Order) ->
+spawn_worker(_F, [], _Pid, _Order) ->
     stop;
-spawn_worker(F, [H|T], Timeout, Pid, Order) ->
+spawn_worker(F, [H|T], Pid, Order) ->
     spawn(?MODULE, pmap_timeout_worker, [F, H, Pid]),
-    spawn_worker(F, T, Timeout, Pid, Order+1).
+    spawn_worker(F, T, Pid, Order+1).
 
 pmap_timeout_worker(F, L, Num, Pid) ->
     Pid ! {Num, [X || X <- L, F(X)]}.
--- W03/advanced/02/list_op.erl
+++ W03/advanced/02/list_op.erl
@@ -3,7 +3,12 @@
 -compile(export_all).
 
 
-% Helpers
+% splits a list L into NrParts, 
+% note that it will never split a list into lists of less items than 2 unless length(L) is specified as the NrParts
+%   e.g. length(L) = 20, NrParts = 19 -> list is split into lists of 2 items
+%   length(L) = 20, NrParts = 20 -> list is split into lists of 1 item
+% the last list in the resulting lists will carry fewer than the preceding if the list is not evenly dividable
+%   e.g. list_op:splitList(lists:seq(1,5),3). -> [[1,2],[3,4],[5]]
 splitList(L, NrParts) ->
     ParLen = round(math:ceil(length(L)/NrParts)), %TODO: split better
     splitListParLen(L, ParLen).
