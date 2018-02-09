2aacd306f4f983577aaa6ea259caeffb0b0739a6
Fredrik <flarsson93@gmail.com>
2018-02-09 17:03:47 +0100

    remove debug expressions

--- W03/advanced/03/ring.erl
+++ W03/advanced/03/ring.erl
@@ -14,34 +14,29 @@ start(N, M) ->
 
 create_ring(N, M, RPid) ->
     Pid = spawn(?MODULE, loop, [null, 0, M, true, RPid]),
-%    io:format("~p -> ",[Pid]),
     Pid ! create_ring_body(Pid, M, 1, N),
     Pid.
 
 create_ring_body(PrevPid, _M, N, N) -> PrevPid;
 create_ring_body(PrevPid, M, CurrentN, N) ->
     Pid = spawn(?MODULE, loop, [PrevPid, 0, M, false, null]),
-%    io:format("~p -> ",[Pid]),
     create_ring_body(Pid, M, CurrentN+1, N).
 
 loop(_NextPid, M, M, FirstBol, RPid) -> % End of the loop
     if 
-        FirstBol ->
+        FirstBol -> % if it is the first created we send the result back
             receive
                 X when is_integer(X) ->
-%                    io:format("~p~n",[X]),
                     RPid ! X
             end;
-        true ->
+        true -> % otherwise we just stop
             stop
     end;
 loop(NextPid, CountRec, M, FirstBol, RPid) ->
     receive
         Pid when is_pid(Pid) -> % The first process needs to know the last created process to complete the ring
-%            io:format("~p - closed chain~n",[self()]),
             loop(Pid, CountRec, M, FirstBol, RPid);
         X when is_integer(X) ->
-%            io:format("~p -> ",[X]),
             NextPid ! X+1,
             loop(NextPid, CountRec+1, M, FirstBol, RPid)
     end.
